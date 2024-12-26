096b7cfefb327c442d6eb45679b4a92811e67513
Sorway <sorwayhd@gmail.com>
2024-12-26 23:04:43 +0100

    🏷️ Removing tags

--- public/javascripts/tags.js
+++
@@ -1,9 +0,0 @@
-function convertToTransparent(color) {
-    if (color.startsWith('#')) {
-        let r = parseInt(color.slice(1, 3), 16);
-        let g = parseInt(color.slice(3, 5), 16);
-        let b = parseInt(color.slice(5, 7), 16);
-        return `rgba(${r}, ${g}, ${b}, 0.5)`;
-    }
-    return color;
-}

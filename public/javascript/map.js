a5a1bbea45b569d88fe95c9524f3fa7ad799ebb7
Sorway <sorwayhd@gmail.com>
2025-01-05 23:12:58 +0100

    Change API URL

--- public/javascript/map.js
+++ public/javascript/map.js
@@ -104,5 +104,5 @@ function updateSidebar() {
 map.on('moveend', updateSidebar);
 
 // Chargement des randonnées et spots
-loadMarkers('http://localhost/api/hikings', hikingIcon, 'hikings', 'tag-hiking', 'Randonnée');
-loadMarkers('http://localhost/api/spots', spotIcon, 'spots', 'tag-spot', 'Spot Photo');
+loadMarkers('https://hikings.jonathan-gp.fr/api/hikings', hikingIcon, 'hikings', 'tag-hiking', 'Randonnée');
+loadMarkers('https://hikings.jonathan-gp.fr/api/spots', spotIcon, 'spots', 'tag-spot', 'Spot Photo');
